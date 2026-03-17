05a0536ee0005efb212c89fb16b6b172d3b64ac4
pskdTeam <154627802+P4Installer@users.noreply.github.com>
2026-03-17 18:19:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -21,14 +21,15 @@ PHOTO_PATH = os.path.join(BASE_DIR, "start.png")
 BOT_TOKEN: Final = "8021165557:AAHfkfkZUQrpbHlZDOaE0cl-FKClC8BLNXY"
 CHANNEL_ID: Final = "@StarGGamerGIFTS"
 SUPPORT_URL: Final = "https://t.me/StarGGamer_official"
-GIFT_PRICE: Final = 55 
+GIFT_PRICE: Final = 50 
 
 GIFTS_MENU = {
     "🧸 Новогодний мишка": {"id": "5956217000635139069", "sticker": "CAACAgIAAxkBAAEQl5BpmzgZK--9COG2FEg7wfNuqxSwYgACF5wAAnHUyEhM_w9CoUqiwjoE"}, 
     "🎄 Новогодняя елка": {"id": "5922558454332916696", "sticker": "CAACAgIAAxkBAAEQl45pmzgXbHlGoAit3lZVhdx7iHt5nwACepEAAjByyEgjzOEYl2IJMDoE"},
     "🧸 Мишка влюбленных": {"id": "5800655655995968830", "sticker": "CAACAgIAAxkBAAEQl4BpmzWFPseGIp1ovJpEAafR2R3KjwAC74MAAtmryUiG3BZvmLqUDDoE"}, 
     "💝 Сердце влюбленных": {"id": "5801108895304779062", "sticker": "CAACAgIAAxkBAAEQl4xpmzfpOkSnX3q6HD0lkq8F9HylXgAC0I8AAtL6yEiWRP-UqnQTRDoE"},
-    "🧸 Девчачий мишка": {"id": "5866352046986232958", "sticker": "CAACAgIAAxkBAAEQs_5prJZKHlJfF65cV0Bfmrn7ELEOHwAC4p4AAr75YUkVQ46Zb1culjoE" }
+    "🧸 Девчачий мишка": {"id": "5866352046986232958", "sticker": "CAACAgIAAxkBAAEQs_5prJZKHlJfF65cV0Bfmrn7ELEOHwAC4p4AAr75YUkVQ46Zb1culjoE" },
+    "🧸 Мишка Патрика": {"id": "5893356958802511476", "sticker": "CAACAgIAAxkBAAEQxRJpuX6G2AU8j068gUCfsqa8UgABbvcAAkGYAAJoh9FJ5T__TtIq7_w6BA" }
 }
 
 class GiftStates(StatesGroup):
